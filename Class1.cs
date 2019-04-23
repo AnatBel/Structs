a31546194b5a8e61959e76779c37c1a1bbdb797f
AnatBel <49661007+AnatBel@users.noreply.github.com>
2019-04-23 14:17:36 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structs
+{
+    struct Student
+    {
+        public string idCard;
+        public string Name;
+        public string Surname;
+        public int age;
+        public gender gndr;
+        public string groupName;
+    }
+    enum gender:byte
+    {
+        male,
+        female,
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input number of students in group");
+            string str = null;
+            int nmbr;
+            while(!int.TryParse(str, out nmbr))
+            {
+                str=Console.ReadLine();
+            }
+            str=null;
+            Student[] gr=new Student[nmbr];
+            for (int i = 0; i < gr.GetLength(0); i++)
+            {
+                Console.Write("number of card:");
+                gr[i].idCard = Console.ReadLine();
+                Console.Write("Name:");
+                gr[i].Name = Console.ReadLine();
+                Console.Write("Surname:");
+                gr[i].Surname = Console.ReadLine();
+                Console.Write("GroupName:");
+               gr[i].groupName = Console.ReadLine();
+
+               Console.Write("Age:");
+               while (!int.TryParse(str, out gr[i].age))
+               {
+                   str = Console.ReadLine();
+               }
+               str = null;
+               Console.Write("Gender:");
+               gr[i].gndr =gender.male;// (gender)Console.ReadLine();
+                
+            }
+            
+        }
+    }
+}
